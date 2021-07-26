557b8936cf64ab8874de7ad2c230ba82412ea8ca
Teddy Randby <tedrandby@gmail.com>
2021-07-25 21:19:44 -0400

    v 0.1

---
+++ lua/lute/config.lua
@@ -0,0 +1,41 @@
+local M = {
+  template_cmd = {
+    cargo = {
+      build = "cargo run",
+      test = "cargo test",
+    },
+    make = {
+      build = "make",
+      test = "make test",
+      run = "./main" -- Your executable here
+    },
+    npm = {
+      start = "npm run start",
+      dev = "npm run dev",
+      test = "npm run test",
+    },
+  },
+}
+
+function M.template_options()
+  local opts = {}
+  for opt, _ in pairs(M.template_cmd) do
+    table.insert(opts, opt)
+  end
+  return opts
+end
+
+
+function M.write_template(template)
+  local file = io.open('.lute', 'w')
+  io.output(file)
+  local base = "local lute = require(\"lute\")\n\nlocal project = {\n\tcmd = {\n"
+  for name, cmd in pairs(M.template_cmd[template]) do
+    base = base .. '\t\t' .. name .. ' = \"' .. cmd .. '\",\n'
+  end
+  base = base .. "\t}\n}\n\nlute.project = project"
+  io.write(base)
+end
+
+
+return M
---
+++ lua/lute/init.lua
@@ -0,0 +1,58 @@
+local lute = require("lute.lute")
+
+-- Some default/example runners
+
+--lua
+-- Doesn't use write_command bc lua code can just be ran w/in nvim
+-- If a lua runtime is intalled on your system,
+-- There's no reason that lua can't be run using write_command
+lute.new_runner(
+  "lua",
+  "%.lua$",
+  function (filename)
+    return "luafile " .. filename
+  end)
+
+--python
+lute.new_runner(
+  "python",
+  "%.py$",
+  function (filename)
+    return lute.write_command("python3", filename)
+  end)
+
+--js
+lute.new_runner(
+  "javascript",
+  "%.js$",
+  function (filename)
+    return lute.write_command("node", filename)
+  end)
+
+--ts
+lute.new_runner(
+  "typecript",
+  "%.ts$",
+  function (filename)
+    return lute.write_command("ts-node", filename)
+  end)
+
+--jest js testing framework
+lute.new_runner(
+  "js/ts-test",
+  {"%.test%.js$", "%.spec%.js$", "%.test%.ts$", "%.spec%.ts$"},
+  function (filename)
+    return lute.write_command("jest", filename)
+  end)
+
+--This will just compile the current file.
+--For languages like rust, a project configuration is more useful.
+lute.new_runner(
+  "rust",
+  "%.rs$",
+  function (filename)
+    return lute.write_command("rustc", filename)
+  end
+)
+
+return lute
